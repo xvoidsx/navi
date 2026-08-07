027a38500a64460b9cd92fbbd70436a0fc6af49f
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-08-07 05:47:43 -0500

    Add nightly browsers installation script

---
+++ scripts/installers/nightly-browsers.sh
@@ -0,0 +1,37 @@
+#/usr/bin/env bash
+# nightly browsers installer
+# ( installs "Nightly" browsers. )
+# ( we keep Firefox Nightly and Brave Nightly around )
+set -e
+x="= = = = = nightly browsers installer = = = = ="
+firefox_nightly_install () {
+  local pkg="firefox-nightly"
+  sudo install -d -m 0755 /etc/apt/keyrings
+  wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+  sudo tee /etc/apt/sources.list.d/mozilla.sources > /dev/null << EOF
+Types: deb
+URIs: https://packages.mozilla.org/apt
+Suites: mozilla
+Components: main
+Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc
+EOF
+  sudo tee /etc/apt/preferences.d/mozilla > /dev/null << EOF
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+EOF
+  sudo apt update && sudo apt install -y "$pkg"
+}
+brave_nightly_install () {
+  curl -fsS https://dl.brave.com/install.sh | CHANNEL=nightly sh
+}
+main () {
+ echo "$x"; sleep 1
+ echo "Installing Firefox Nightly..."; sleep 1
+ firefox_nightly_install
+ echo "Installing Brave Browser Nightly..."; sleep 1
+ brave_browser_install
+ echo "Nightly browsers installed."; sleep 1
+}
+# - - - - - |
+main
