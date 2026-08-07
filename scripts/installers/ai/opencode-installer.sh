9f5ae536fc48766741e4f775f798c049315139fa
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-08-07 06:14:18 -0500

    Create opencode-installer.sh

---
+++ scripts/installers/ai/opencode-installer.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+# opencode installer
+# ( installs opencode )
+##################################
+set -e
+x="= = = = = opencode installer = = = = ="
+install_oc () {
+  local link="https://opencode.ai/install"
+  curl -fsSL "$link" | bash
+  # append to bashrc
+  echo 'export PATH="$HOME/.opencode/bin:$PATH"' >> $HOME/.bashrc
+}
+# - - - - - |
+main
