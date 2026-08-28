868bbfe9e46756a1cda643e977a312089a8c940a
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-08-28 12:21:29 -0500

    update gifpaperslain setter
    for setting gifpapers (animated wallpapers)

--- scripts/system/gifpaperslain.sh
+++ scripts/system/gifpaperslain.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # gifpaperslain
 #     by rav3ndust.xyz (xvoidsx)
-# A simple way to set animated wallpapers in naviOS.
+# A simple way to set animated wallpapers in navi
 # Uses 'mpvpaper' and 'zenity' on the backend.
 set -e
 x="gifpaperslain"
+# force wayland env vars
+if [ -z "$WAYLAND_DISPLAY" ]; then
+	export WAYLAND_DISPLAY="wayland-0"
+fi
 notifier () {
 	notify-send -u low -t 2000 --transient "$x" "Displaying available animated wallpaper selections."
 }
 kill_last_instance () {
 	# kills the last instance of mpvpaper if one is running
-	if pgrep -x "mpvpaper" > /dev/null; then
-		killall mpvpaper
-	fi
+	killall mpvpaper || true
 }
 main () {
 	notifier
@@ -23,5 +25,4 @@ main () {
 	kill_last_instance
 	mpvpaper ALL -o "loop panscan=1" "$gifpaper" 
 }
-# - - - - - |
 main
